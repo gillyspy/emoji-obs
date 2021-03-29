605fbc7834fe2fbc6f41795f26cbca41c56faad7
gillyspy <gillyspy@gmail.com>
2021-03-29 08:06:46 -0400

    initializing project with webpack

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,1 +1,28 @@
-alert('hello world');
+//alert('hello world!!!');
+
+import { EmojiButton } from '@joeattardi/emoji-button';
+
+const picker = new EmojiButton();
+const trigger = document.querySelector('.trigger');
+const target = document.querySelector('.target');
+
+picker.on('emoji', selection => {
+  target.innerHTML = selection.emoji;
+});
+
+trigger.addEventListener('click', () => picker.togglePicker(trigger));
+
+class Favorites {
+  constructor(){
+
+  }
+
+  stashFavorite(){
+
+  }
+}
+
+
+if( module.hot) {
+  module.hot.accept();
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,48 @@
-module.exports = {
-  entry : './app/asset/scripts/App.js'
+const currentTask = process.env.npm_lifecycle_event;
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const fse = require('fs-extra');
+
+let pages = fse.readdirSync('./app').filter(function(f){
+    return f.endsWith('.html');
+}).map(function(page){
+    return new HtmlWebpackPlugin({
+        filename : page,
+        template : `./app/${page}`
+    })
+});
+
+let config = {
+    entry: './app/assets/scripts/App.js',
+    plugins : pages,
+    module : {
+        rules : [ //
+        // cssConfig
+         ]
+    }
 }
+
+if (currentTask == 'dev') {
+    config.output = {
+        filename : 'bundled.js',
+        path : path.resolve( __dirname, 'app' ) //multiplatform friendly
+    };
+    config.devServer = {
+        before : function(app, server){
+            server._watch('./app/**/*.html')
+
+        },
+        contentBase : path.join(__dirname, 'app'),
+        hot : true,
+        host : '10.0.0.2',
+        disableHostCheck : true,
+        // host :  '0.0.0.0',
+        port : 3000
+
+    }
+    config.mode = 'development'
+
+}
+
+module.exports = config;
 
