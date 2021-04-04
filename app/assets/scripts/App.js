bb4ba6e64e71e13156395ef7ba27e8cd7c7485ef
gillyspy <gillyspy@gmail.com>
2021-04-04 16:38:48 -0400

    added plugin for sticky on new emojis also caching config emojis as recents in picker

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,183 +5,239 @@ import Animation from './modules/Animation';
 import Init from './modules/Init.js';
 import Draw from 'draw-on-canvas';
 
-const picker = new EmojiButton({
-  theme : 'dark',
-  rows : 2,
-  emojisPerRow : 11,
-  showRecents : true,
-  initialCategory : 'recents',
-  showVariants : true,
- /* position: {
-    top: '100',
-    left: '0'
-  },*/
-   position: 'bottom-start',
-  emojiSize : '60px',
-  showPreview : false
-});
+Array.prototype.unique = function (property) {
+  var a = this.concat();
+  if (typeof property !== 'undefined') {
+    for (var i = 0; i < a.length; ++i) {
+      for (var j = i + 1; j < a.length; ++j) {
+        if (a[i][property] === a[j][property])
+          a.splice(j--, 1);
+      }
+    }
+  } else {
+    for (var i = 0; i < a.length; ++i) {
+      for (var j = i + 1; j < a.length; ++j) {
+        if (a[i] === a[j])
+          a.splice(j--, 1);
+      }
+    }
+  }
+  return a;
+} // unique
+
+
 const myFavs = new Favorites();
 const myAnimation = new Animation();
+
 $(document).ready(function () {
-  const trigger = document.querySelector('.trigger');
-  var $highlight = [];
-  const target = $('#target');
+    const stickyButton = $('#stickybutton');
+    const picker = new EmojiButton({
+      theme          : 'dark',
+      rows           : 2,
+      emojisPerRow   : 11,
+      showRecents    : true,
+      initialCategory: 'recents',
+      showVariants   : true,
+      /* position: {
+         top: '100',
+         left: '0'
+       },*/
+      position       : 'bottom-start',
+      emojiSize      : '60px',
+      showPreview    : false,
+      recentsCount   : 50,
+      zIndex         : 10000,
+      plugins        : [Init.setDefaults()], //
+      stickyHandler  : {
+        element: stickyButton,
+        event  : 'click'
+      }, //
+      kwijibo        : 'did my property get set?'
+
+    });
+
+
+    const trigger = document.querySelector('.trigger');
+    var $highlight = [];
+    const target = $('#target');
 //$target.fadeOut(40000);//default
-  const stickyButton = $('#stickybutton');
-  const $outer = $('.outer');
-  const $afk = $('.AFK');
-  const $emojipreview = $('#emojipreview');
-  const $history = $('#history');
-  const $moveHistory = $('#moveHistory');
-  const $hideButton = $('#hideEmoji');
-  myAnimation.init(target);
-  const $drawing = $('#drawing').hide();
-  const $drawButton = $('button.draw');
-  const $drawButton2 = $('button.nogrid');
-  const $Grid = $('#Grid');
-  $Grid.find('.grid').css('min-height', Init.visibleHeight+'px');
-  const Drawing  = new Draw($drawing.find('#drawingPane')[0], window.innerWidth, Init.visibleHeight, Init.canvas);
+    const $outer = $('.outer');
+    const $afk = $('.AFK');
+    const $emojipreview = $('#emojipreview');
+    const $history = $('#history');
+    const $moveHistory = $('#moveHistory');
+    const $hideButton = $('#hideEmoji');
+    myAnimation.init(target);
+    const $drawing = $('#drawing').hide();
+    const $drawButton = $('button.draw');
+    const $drawButton2 = $('button.nogrid');
+    const $Grid = $('#Grid');
+    $Grid.find('.grid').css('min-height', Init.visibleHeight + 'px');
+    const Drawing = new Draw($drawing.find('#drawingPane')[0], window.innerWidth, Init.visibleHeight, Init.canvas);
+    var emojiCache = JSON.parse(localStorage.getItem('emojiPicker.recent'));
 
-  $drawing.on('click', 'button', function(){
-    let $this = $(this);
-    if($this.hasClass('reset')){
-      return Drawing.reset();
-    }else if($this.hasClass('stop')){
-      $drawButton.click();
-    } else if( $this.hasClass('nogrid')){
-      $('#Grid').toggle();
-    } else if( $this.hasClass('redDraw')){
-      //change color
-      Drawing.strokeColor ='red';
-    }else if( $this.hasClass('orangeDraw')){
-      //change color
-      Drawing.strokeColor = 'orange';
-    }else if( $this.hasClass('blueDraw')){
-      //change color
-      Drawing.strokeColor = 'blue';
-    }else if( $this.hasClass('greenDraw')){
-      //change color
-      Drawing.strokeColor = 'green';
-    }else if( $this.hasClass('purpleDraw')){
-      //change color
-      Drawing.strokeColor = 'purple';
-    }
-  });
-
-$('#MessageSource').on('keyup click','form', function(){
-  $('#MessageTarget').text( $('#Message').val());
-
-});
-  $drawButton.on('click', ()=>{
-    if( $drawing.is(':hidden') ){
-      $drawing.show();
-      //$('#Grid').show();
-    } else {
-      $drawing.hide();
-      $('#Grid').hide();
-    }
-  });
-  $drawButton.click();
-  $afk.on('click', function (ev) {
-    let $this = $('#afk');
-    $this.toggle();
-    if ($this.is(':hidden')) {
-      $('#' + '✌🏻').click();
-    } else {
-      $('#' + '💤').click();
-      stickyButton.click();
-    }
-    ev.preventDefault();
-    return false;
-  });
-
-  $hideButton.on('click', function(){
-    //archiveFave();
-    var fav= myFavs.recallFave(target.text() );
-    myAnimation.toggleHide(fav.sticky);
-  })
-
-  stickyButton.on('click', (ev) => {
-    //make the current emoji sticky
-    var fav= myFavs.recallFave(target.text() );
-    if(fav){
-      fav.sticky = !fav.sticky;
-      if( fav.sticky){
-          myAnimation.removeAnimation();
-      } else {
-        myAnimation.restartAnimation()
+    $drawing.on('click', 'button', function () {
+      let $this = $(this);
+      if ($this.hasClass('reset')) {
+        return Drawing.reset();
+      } else if ($this.hasClass('stop')) {
+        $drawButton.click();
+      } else if ($this.hasClass('nogrid')) {
+        $('#Grid').toggle();
+      } else if ($this.hasClass('redDraw')) {
+        //change color
+        Drawing.strokeColor = 'red';
+      } else if ($this.hasClass('orangeDraw')) {
+        //change color
+        Drawing.strokeColor = 'orange';
+      } else if ($this.hasClass('blueDraw')) {
+        //change color
+        Drawing.strokeColor = 'blue';
+      } else if ($this.hasClass('greenDraw')) {
+        //change color
+        Drawing.strokeColor = 'green';
+      } else if ($this.hasClass('purpleDraw')) {
+        //change color
+        Drawing.strokeColor = 'purple';
       }
+    });
 
+    $('#MessageSource').on('keyup click', 'form', function () {
+      $('#MessageTarget').text($('#Message').val());
+
+    });
+    $drawButton.on('click', () => {
+      if ($drawing.is(':hidden')) {
+        $drawing.show();
+        //$('#Grid').show();
+      } else {
+        $drawing.hide();
+        $('#Grid').hide();
+      }
+    });
+    $drawButton.click();
+    $afk.on('click', function (ev) {
+      let $this = $('#afk');
+      $this.toggle();
+      if ($this.is(':hidden')) {
+        $('#' + '✌🏻').click();
+      } else {
+        $('#' + '💤').click();
+        stickyButton.click();
+      }
+      ev.preventDefault();
+      return false;
+    });
+
+    $hideButton.on('click', function () {
+      //archiveFave();
+      var fav = myFavs.recallFave(target.text());
+      myAnimation.toggleHide(fav.sticky);
+    })
+
+    stickyButton.on('click', function (ev) {
+      //make the current emoji sticky
+      var fav = myFavs.recallFave(target.text());
+      if (fav) {
+        fav.sticky = !fav.sticky;
+        if (fav.sticky) {
+          myAnimation.removeAnimation();
+        } else {
+          myAnimation.restartAnimation()
+        }
+
+      }
+      //ev.preventDefault();
+      return false;
+    });
+
+    //
+    const highlightFave = function (emoji) {
+      emoji = emoji || target.text();
+      $highlight = $('#' + emoji).addClass('highlight');
     }
-    ev.preventDefault();
-    return false;
-  });
+    var archiveFave = function () {
+      $('.highlight').removeClass('highlight');
+      $highlight = [];
 
-  var highlightFave = function (emoji) {
-    emoji = emoji || target.text();
-    $highlight = $('#' + emoji).addClass('highlight');
-  }
-  var archiveFave = function () {
-    $('.highlight').removeClass('highlight');
-    $highlight = [];
-
-    //add unique entry to the history
-    if ($('#' + target.text()).length === 0) {
-      $history
+      //add unique entry to the history
+      if ($('#' + target.text()).length === 0) {
+        $history
         .prepend('<span class="letters"><button id="' + target.text() + '" class="history">' + target.text() + '</button></span>');
     }
 
-    //update size of icons
-    var histCt = $('.history').length;
-    var size = (675 / 50) + 5;
-    if (histCt < 14) {
-      size = Math.max(size, 50);
+      //update size of icons
+      var histCt = $('.history').length;
+      var size = (675 / 50) + 5;
+      if (histCt < 14) {
+        size = Math.max(size, 50);
+      }
+      $('.history').css('font-size', size + 'px');
+    } //archiveFave
+
+    /**********************/
+    Init.init.forEach(function (emoji, i, a) {
+      archiveFave();
+
+      var fave = myFavs.stashIt(emoji);
+      fave.sticky = !!(Init.stickyInit[fave.emoji]);
+      let name = fave.name || emoji;
+
+      //put our emojis into a nice format
+      a[i] = {
+        emoji: emoji,
+        name : 'myInit' + i,
+        key  : name
+      };
+
+      if ((i + 1) === Init.init.length) {
+        if (fave.sticky) {
+          myAnimation.removeAnimation();
+        } else {
+          myAnimation.addAnimation();
+        }
+
+      }
+      target.text(emoji);
+      //try to highlight in history
+      highlightFave(target.text());
+    }); // init for each
+
+    //add to the emojiPicker cache
+    if (emojiCache.length) {
+      var newArray = Init.init.concat(emojiCache).unique('emoji').slice(0, 50);
+
+      localStorage.setItem(
+        "emojiPicker.recent",
+        JSON.stringify(newArray)
+      );
     }
-    $('.history').css('font-size', size + 'px');
-  } //archiveFave
 
-  /**********************/
-  Init.init.forEach((emoji) => {
-    archiveFave();
+    stickyButton.click(); //initialize sticky  for the last emoji
 
-    var fave = myFavs.stashIt(emoji);
-    fave.sticky = !!(Init.stickyInit[fave.emoji])
+    const injectSelection = function (selection) {
+      //a highlighted one means we have nothing to archive
+      archiveFave();
 
-    if (fave.sticky) {
-      myAnimation.removeAnimation();
-    } else {
-      myAnimation.addAnimation();
+      var fave = myFavs.stashIt(selection.emoji);
+
+      if (fave.sticky) {
+        myAnimation.removeAnimation();
+      } else {
+        myAnimation.addAnimation();
+      }
+
+      target.text(selection.emoji);
+
+      //try to highlight in history
+      highlightFave(target.text());
+
+      return false;
     }
-    target.text(emoji);
-
-    //try to highlight in history
-    highlightFave(target.text());
-  }); // init for each
-
-  stickyButton.click(); //initialize sticky  for the last emoji
-
-  const injectSelection = function (selection) {
-    //a highlighted one means we have nothing to archive
-    archiveFave();
-
-    var fave = myFavs.stashIt(selection.emoji);
-
-    if (fave.sticky) {
-      myAnimation.removeAnimation();
-    } else {
-      myAnimation.addAnimation();
-    }
-
-    target.text(selection.emoji);
-
-    //try to highlight in history
-    highlightFave(target.text());
-
-    return false;
-  }
   /*******************/
   picker.on('emoji', selection => {
     // reset animation on every new emoji
+
     return injectSelection(selection);
 
   });
@@ -335,8 +391,11 @@ $('#MessageSource').on('keyup click','form', function(){
 
     }
 
-  })
-});
+  });
+    //localStorage.setItem('emojiPicker.recent')
+
+  }
+);
 
 if( module.hot) {
   module.hot.accept();
--- app/assets/scripts/modules/Init.js
+++ app/assets/scripts/modules/Init.js
@@ -1,17 +1,96 @@
- const Init = {
-   init : ['💩', '🔇', '🏳️', '👍🏻', '🔥', '✅', '❌', '👂🏻', '🧠',
-     '🐧', '🦈', '☁️', '⛈️', '🌮', '🎧', '🔋', '🛠️',
-     '🧲', '📅', '🛏️', '💤', '⁉️', '‍️⚠️', '🔫', '🤦🏻‍♂️', '🥓',
-     '👋🏻', '✋🏻', '💡', '✌🏻'],
-   stickyInit : {
-     "🔇": true
-   },
-   canvas : {
-    backgroundColor: "rgba(85,0,255,0)", //"#5500ff",
-    strokeColor:"orange",
-    strokeWeight:10
-  },
-   visibleHeight : 575
- };
+const Init = {
+    init         : ['💩', '🔇', '🏳️', '👍🏻', '🔥', '✅', '❌', '👂🏻', '🧠',
+      '🐧', '🦈', '☁️', '⛈️', '🌮', '🎧', '🔋', '🛠️',
+      '🧲', '📅', '🛏️', '💤', '⁉️', '‍️⚠️', '🔫', '🤦🏻‍♂️', '🥓',
+      '👋🏻', '✋🏻', '💡', '✌🏻'],
+    stickyInit   : {
+      "🔇" : true,
+      '💤' : true,
+      '✌🏻': true
+    },
+    canvas       : {
+      backgroundColor: "rgba(85,0,255,0)", //"#5500ff",
+      strokeColor    : "orange",
+      strokeWeight   : 10
+    },
+    visibleHeight: 575,
+    setDefaults  : function () {
+      // track the picker being called for the first time
+      //if it is NOT the first time the picker is being rendered then render a button that allows the emoji to be made "sticky"
+      return {
+        render: function (picker) {
+          /*
+          if no emoji is selected then nothing happens -- no sticky pin applied either.
+           */
+          var doHandlerOnHide = false; // temporary cache on whether to set the selected emoji as sticky.
+          var isFirstRunComplete = false;
 
- export default Init;
+          //ONLY if there is a stickyHandler then we can evaluate whether or not to apply a sticky pin
+          if (typeof picker.options.stickyHandler !== 'object') {
+            //stickyHandler is a function of what to do when an object is marked sticky
+            return;
+          } else {
+            //add the stickyHandler
+            picker.on('emoji', function () {
+              // console.log(picker.options);
+              //if the sticky button is pressed when emoji is selected then set the temporary flag
+              if (picker.options.makeEmojiSticky) {
+                doHandlerOnHide = true;
+              } else {
+                //unset the temporary flag
+                doHandlerOnHide = false
+              }
+              return;
+            });
+
+            picker.on('hidden', function () {
+              if (doHandlerOnHide) {
+                picker.options.stickyHandler['element'].trigger(picker.options.stickyHandler['event']);
+              }
+              //always set the doHandlerOnHide to false here as this is a temporary flag
+              doHandlerOnHide = false;
+              return;
+            })
+          }
+          //if it is the first time then populate the recents category with several default emojis
+          if (!isFirstRunComplete) {
+            //the list of defaults is optionally provided by the user. If there are no defaults then ??
+            isFirstRunComplete = true;
+
+            if (typeof picker.options.makeEmojiSticky === 'undefined') {
+              picker.options.makeEmojiSticky = false;
+            }
+            //scan through the list of desired emojis and make the history
+          }
+          //always render the sticky button
+          const button = document.createElement('button');
+          button.innerHTML = '📌';
+
+          picker.options
+          button.addEventListener('click', () => {
+            if (picker.options.makeEmojiSticky) {
+              //remove the class
+              button.classList.remove('pressed');
+              //set the option
+            } else {
+              //add the class
+              button.classList.add('pressed');
+              //set the option
+            }
+            //reverse the option
+            picker.options.makeEmojiSticky = !picker.options.makeEmojiSticky;
+
+            //alert('hello from the 📌 ' + picker.options.makeEmojiSticky);
+            console.log('what is THIS???', this);
+            console.log('what is picker???', picker);
+          });
+          return button;
+
+        } //setDefaults emoji plugin
+      };
+    }
+  }
+;
+
+
+export default Init;
