688c514f4912506a8f6a167217dda68a80d73d09
gillyspy <gillyspy@gmail.com>
2021-04-09 05:53:16 -0400

    fix double triggering on some elements

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -552,25 +552,25 @@ J$(document).ready(function ($) {
 
   $moveHistory.on('click', function(){
     //$('.history').hide();
-   // if ($history.parent('#col1a').length > 0) {
+    // if ($history.parent('#col1a').length > 0) {
 
     //send all the floating big history back home
     $('.history--draggable')
       .removeClass('history--hidden')
-      .trigger('dblclick');
+    $('span.history--draggable').trigger('dblclick');
 
-     // $history.appendTo($('#letters'));
-      myAnimation.anime.timeline({loop: 1})
-        .add({
-          targets   : '#history .history',
-          scale     : [0.3, 1],
-          opacity   : [0, 1],
-          translateZ: 0,
-          easing    : "easeOutExpo",
-          duration  : 600,
-          delay     : (el, i) => 70 * (i + 1)
-        })
-        .add({
+    // $history.appendTo($('#letters'));
+    myAnimation.anime.timeline({loop: 1})
+      .add({
+        targets   : '#history .history',
+        scale     : [0.3, 1],
+        opacity   : [0, 1],
+        translateZ: 0,
+        easing    : "easeOutExpo",
+        duration  : 600,
+        delay     : (el, i) => 70 * (i + 1)
+      })
+      .add({
         targets : '.text-wrapper .line',
         scaleX  : [0, 1],
         opacity : [0.5, 1],
@@ -579,12 +579,12 @@ J$(document).ready(function ($) {
         offset  : '-=700',
         delay   : (el, i, l) => 80 * (l - i)
       }).add({
-        targets : '.text-wrapper .line',
-        opacity : 0,
-        duration: 1000,
-        easing  : "easeOutExpo",
-        delay   : 1000
-      });
+      targets : '.text-wrapper .line',
+      opacity : 0,
+      duration: 1000,
+      easing  : "easeOutExpo",
+      delay   : 1000
+    });
   /*  }else{
         $history.appendTo($('#col1a'));
       myAnimation.anime.timeline({loop : 1})
